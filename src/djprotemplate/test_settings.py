eac62abcdef0323a3a06acd495d8ed656d1367c1
anshum4n <anshum4n@github>
2013-12-12 19:58:39 +0530

    Fixed Project Name Generation and Path Issues

--- src/djprotemplate/test_settings.py
+++
@@ -1,9 +0,0 @@
-# pylint: disable=W0401,W0614
-from settings import *
-
-DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
-DATABASES['default']['NAME'] = os.path.join(OUT_DIR, 'test.db')
-SECRET_KEY = 'testsecretkeyshouldntbeusedinproduction'
-
-INSTALLED_APPS = INSTALLED_APPS + ('django_nose',)
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
