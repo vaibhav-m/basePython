8cbe27c4256ea716add1215bd3cd8442152123b2
anshum4n <anshum4n@github>
2013-12-13 14:38:14 +0530

    Fixed Test Settings by Adding Import

--- etc/templates/test_settings.py
+++ etc/templates/test_settings.py
@@ -1,5 +1,6 @@
 # pylint: disable=W0401,W0614
 from settings import *
+import os
 
 DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 DATABASES['default']['NAME'] = os.path.join(OUT_DIR, 'test.db')
