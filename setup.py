2fa1b81240a36cbc6606106fc11914cf8c1aab18
trojanware <silverspade_vaibhav@hotmail.com>
2015-05-22 20:35:11 +0530

    hello world!

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="my_custom_project",
+    name="basePython",
     version="0.0.1",
     packages=find_packages('src'),
     package_dir={'': 'src'},
